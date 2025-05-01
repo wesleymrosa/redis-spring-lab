6b5798c1ad4438f4f3f27a3be6291a23827e3508
wesleymrosa <wesleymrosa@gmail.com>
2025-05-01 11:48:56 -0300

    Primeiro commit: projeto didático desenvolvido para estudo e demonstração prática do uso de cache com Redis em aplicações Spring Boot, utilizando Docker para orquestração de ambientes.

---
+++ src/main/java/br/wesley/redis/RedisSpringLabApplication.java
@@ -0,0 +1,13 @@
+package br.wesley.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/wesley/redis/RedisSpringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wesley.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
