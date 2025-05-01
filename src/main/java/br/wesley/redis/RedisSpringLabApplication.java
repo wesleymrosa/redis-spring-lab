6197009dcd38d51566b267765f6e092d07456408
wesleymrosa <wesleymrosa@gmail.com>
2025-05-01 11:52:07 -0300

    Incluído suporte a cache com @EnableCaching na classe principal da aplicação.

--- src/main/java/br/wesley/redis/RedisSpringLabApplication.java
+++ src/main/java/br/wesley/redis/RedisSpringLabApplication.java
@@ -2,12 +2,12 @@ package br.wesley.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisSpringLabApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RedisSpringLabApplication.class, args);
 	}
-
 }
