4e43ced6b4f44ddb4244f974c87027a8740b4607
wesleymrosa <wesleymrosa@gmail.com>
2025-05-01 12:05:07 -0300

    Criado VeiculoDTO para transferência segura de dados entre camadas.

---
+++ src/main/java/br/wesley/redis/dto/VeiculoDTO.java
@@ -0,0 +1,20 @@
+package br.wesley.redis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VeiculoDTO {
+
+    private String placa;
+    private String modelo;
+    private String oficina;
+    private LocalDate dataEntrega;
+}
